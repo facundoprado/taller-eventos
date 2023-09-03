e7a1c73f2ed0e928d3c43cb6dde9d992d50b9c24
facundoprado <facupradoromero123@gmail.com>
2023-09-03 16:31:40 -0300

    Añadir eventos al boton y al div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.getElementById("saludarButton").onclick = function () {
+    alert("Hola!");
+  };
+  
+document.querySelector("div").addEventListener("click", function () {
+    alert("Hola! Soy el div");
+});
+  
